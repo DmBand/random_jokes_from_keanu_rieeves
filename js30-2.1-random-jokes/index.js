33ad07be7ee73d3b786c1550583266678cd00a23
DmBand <d.bandysik@gmail.com>
2022-02-10 21:47:14 +0300

    feat: add translation function

--- js30-2.1-random-jokes/index.js
+++ js30-2.1-random-jokes/index.js
@@ -1,27 +1,78 @@
+'use strict'
+import i18Obj from './translate.js';
+
 const url = 'https://type.fit/api/quotes';
 const content = document.querySelector('.text');
-const auth = document.querySelector('.author')
 const button = document.querySelector('.button');
 const image = document.querySelector('.keanu');
+const ru = document.querySelector('.ru');
+const en = document.querySelector('.en');
 
 
-async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    showData(data);
-    console.log(data)
-}
+// async function getData() {
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     showData(data);
+//     console.log(data)
+// }
+
+// function showData(data) {
+//     let index = Math.floor(Math.random() * 1000);
+//     content.textContent = data[index].text;
+// }
+
+// button.addEventListener('click', () => {
+//     getData();
+//     image.classList.toggle('active');
+// })
+
+// getData()
+
+
+
+async function getQuotes() {  
+    let quotes;
+    if (en.classList.contains('active')) {
+        quotes = './assets/quotes-en.json';
+    } else if (ru.classList.contains('active')) {
+        quotes = './assets/quotes-ru.json';
+    }
+    const res = await fetch(quotes);
+    const data = await res.json(); 
+    console.log(data);
+    showData(data)
+  }
+getQuotes();
 
 function showData(data) {
-    let index = Math.floor(Math.random() * 1000);
+    let index = Math.floor(Math.random() * (data.length));
     content.textContent = data[index].text;
-    auth.textContent = data[index].author;
 }
 
 button.addEventListener('click', () => {
-    getData();
-    image.classList.toggle('active')
-
+    getQuotes();
+    image.classList.toggle('active');
 })
 
-getData()
+
+
+// ____TRANSLATION____
+
+function getTranslate(lang) {
+    const dataList = document.querySelectorAll('[data-i18n]');
+    dataList.forEach((el) => {
+        el.textContent = i18Obj[lang][el.dataset.i18n]
+    });
+}
+
+ru.addEventListener('click', () => {
+    getTranslate('ru');
+    en.classList.remove('active');
+    ru.classList.add('active');
+})
+
+en.addEventListener('click', () => {
+    getTranslate('en');
+    ru.classList.remove('active');
+    en.classList.add('active');
+})
---
+++ js30-2.1-random-jokes/translate.js
@@ -0,0 +1,48 @@
+const i18Obj = {
+    'en': {
+        'button': 'Get a quote from Keanu',
+		"cite1": "I hate it when people lie to me. And I can't stand it, especially for no reason.",
+		"cite2": "It is always pleasant to communicate with women. They are joyful and mysterious. If a man managed to make her laugh, then he will see the most beautiful creature that exists in the world.",
+		"cite3": "The pleasure of work does not depend on the genre, it depends on who you work with, what kind of relationship you have with the actors, with the director, with the material.",
+		"cite4": "To get what you want out of life, you must first determine what you want.",
+		"cite5": "Life is cruel, but everyone has the right to choose. A man can break and can get stronger.",
+		"cite6": "Difficulties shape personality. A person needs to be grateful for all the difficult situations that life has presented him. They are the ones that build character.",
+		"cite7": "Money is money. It's nice when there is something to pay for an apartment, in a restaurant, etc., there is a banal saying - happiness is not in money, but for them you buy the freedom to live the life you like.",
+		"cite8": "Something as simple as paying attention can take up a lot of your time.",
+		"cite9": "It seems to me that even if people are not in the same bodies, they deserve to meet after death.",
+		"cite10": "The most dangerous person is the one who is afraid of his own shadow.",
+		"cite11": "I don't consider myself beautiful or sexy. But that doesn't mean I'm hopeless.",
+		"cite12": "Although I confess that my knees, of course, are no longer as flexible as before, so I have to be more careful and behave more rationally.",
+		"cite13": "I try not to think about my life because I don't have a life. I need therapy.",
+		"cite14": "The acting life does not tire me, we are real wandering minstrels.",
+		"cite15": "All my novels are bright, but short-lived. I don't see anything wrong with that. I don't promise eternal love.",
+		"cite16": "I like it when films make us look at ourselves from the outside, ask ourselves questions: who are we and how do we live?",
+		"cite17": "My life outside of work is quite ordinary and unremarkable.",
+		"cite18": "Relationships assume that each of the partners will take risks. Therefore, you need to be open to girls.",
+		"cite19": "I hope that every film in which I am busy will bring joy and pleasure to people, and what will happen after that, I cannot predict."
+    },
+        'ru': {
+        'button': 'Получить цитату от Киану',
+        "cite1": "Ненавижу, когда люди мне лгут. И терпеть не могу насилие, особенно без причины.",
+		"cite2": "С женщинами всегда приятно общаться. Они радостны и таинственны. Если мужчине удалось заставить ее смеяться, то он увидит самое прекрасное существо, что есть в мире.",
+		"cite3": "Удовольствие от работы не зависит от жанра, это зависит от того, с кем ты работаешь, какие отношения складываются с актерами, с режиссером, с материалом.",
+		"cite4": "Чтобы получить от жизни то, чего хочешь, необходимо в первую очередь определить, а чего же ты хочешь.",
+		"cite5": "Жизнь жестокая, но каждый вправе выбирать. Человек может сломаться и может стать сильнее.",
+		"cite6": "Трудности формируют личность. Человеку нужно быть благодарным за все сложные ситуации, которые преподнесла ему жизнь. Именно они закаляют характер.",
+		"cite7": "Деньги и есть деньги. Приятно, когда есть чем платить за квартиру, в ресторане и т.д., есть банальная поговорка - не в деньгах счастье, но за них ты покупаешь свободу жить той жизнью, какая тебе нравится.",
+		"cite8": "Такое простое действие, как обращать внимание, может отнять у вас много времени.",
+		"cite9": "Мне кажется, что, даже если люди не будут находиться в тех же самых телах, они заслужили, чтобы встретиться после смерти.",
+		"cite10": "Самый опасный человек тот, кто боится собственной тени.",
+		"cite11": "Я не считаю себя красивым или сексуальным. Но это не значит, что я безнадежен.",
+		"cite12": "Хотя признаюсь, что колени мои, конечно, уже не такие гибкие, как раньше, так что приходится быть осторожнее и более рационально себя вести.",
+		"cite13": "Я стараюсь не думать о своей жизни, потому что у меня нет жизни. Мне нужна терапия.",
+		"cite14": "Жизнь актерская меня не утомляет, мы - настоящие странствующие менестрели.",
+		"cite15": "Все мои романы яркие, но кратковременные. Не вижу в этом ничего плохого. Я ведь и не обещаю вечную любовь.",
+		"cite16": "Мне нравится, когда фильмы заставляют нас посмотреть на самих себя со стороны, задать себе вопросы: кто мы такие и как живем?",
+		"cite17": "Моя жизнь вне работы вполне обычна и ничем не примечательна.",
+		"cite18": "Отношения предполагают, что каждый из партнеров будет рисковать. Поэтому нужно быть открытым для девушек.",
+		"cite19": "Надеюсь, что каждый фильм, в котором я занят, принесет людям радость и удовольствие, а что будет после этого, я не могу предугадать."
+    }
+}
+
+export default i18Obj;
